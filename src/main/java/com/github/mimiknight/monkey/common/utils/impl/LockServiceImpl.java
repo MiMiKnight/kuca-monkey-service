eca027253ae51462be2ff91c73214b41905942a6
MiMiKnight <victor2015yhm@gmail.com>
2023-07-28 22:19:22 +0800

    【关联单号】US202307050831 【提交描述】LockService增加无返回值的自锁方法

--- src/main/java/com/github/mimiknight/monkey/common/utils/impl/LockServiceImpl.java
+++ src/main/java/com/github/mimiknight/monkey/common/utils/impl/LockServiceImpl.java
@@ -32,14 +32,14 @@ public class LockServiceImpl implements LockService {
     /**
      * 执行
      *
-     * @param lockName   锁名字
-     * @param getLock    获取锁的代码
-     * @param lockedCode 被锁的代码
+     * @param lockName    锁名
+     * @param getLockCode 获取锁的代码
+     * @param lockedCode  被锁的代码
      * @return {@link T}
      */
-    private <T> T execute(String lockName, Predicate<String> getLock, Supplier<T> lockedCode) {
+    private <T> T execute(String lockName, Predicate<String> getLockCode, Supplier<T> lockedCode) {
         // 如果获取锁失败则抛出异常
-        if (!getLock.test(lockName)) {
+        if (!getLockCode.test(lockName)) {
             log.info("Failed to get the lock,lock = {}", lockName);
             throw new ServiceException(ErrorReturn.GET_LOCK_FAILED);
         }
@@ -54,15 +54,51 @@ public class LockServiceImpl implements LockService {
         }
     }
 
+    /**
+     * 执行
+     *
+     * @param lockName    锁名
+     * @param getLockCode 获取锁的代码
+     * @param lockedCode  被锁的代码
+     */
+    private void execute(String lockName, Predicate<String> getLockCode, Runnable lockedCode) {
+        // 如果获取锁失败则抛出异常
+        if (!getLockCode.test(lockName)) {
+            log.info("Failed to get the lock,lock = {}", lockName);
+            throw new ServiceException(ErrorReturn.GET_LOCK_FAILED);
+        }
+        try {
+            lockedCode.run();
+        } finally {
+            try {
+                redisLockService.unlock(lockName);
+            } catch (Exception e) {
+                log.error("Failed to release lock manually,lockName = {},error = {}", lockName, e.getMessage());
+            }
+        }
+    }
+
     @Override
     public <T> T doTryLock(String lockName, long waitTime, TimeUnit unit, Supplier<T> lockedCode) {
-        Predicate<String> getLock = key -> redisLockService.tryLock(key, waitTime, unit);
-        return execute(lockName, getLock, lockedCode);
+        Predicate<String> getLockCode = key -> redisLockService.tryLock(key, waitTime, unit);
+        return execute(lockName, getLockCode, lockedCode);
+    }
+
+    @Override
+    public void doTryLock(String lockName, long waitTime, TimeUnit unit, Runnable lockedCode) {
+        Predicate<String> getLockCode = key -> redisLockService.tryLock(key, waitTime, unit);
+        execute(lockName, getLockCode, lockedCode);
     }
 
     @Override
     public <T> T doLock(String lockName, Supplier<T> lockedCode) {
-        Predicate<String> getLock = key -> redisLockService.lock(key);
-        return execute(lockName, getLock, lockedCode);
+        Predicate<String> getLockCode = key -> redisLockService.lock(key);
+        return execute(lockName, getLockCode, lockedCode);
+    }
+
+    @Override
+    public void doLock(String lockName, Runnable lockedCode) {
+        Predicate<String> getLockCode = key -> redisLockService.lock(key);
+        execute(lockName, getLockCode, lockedCode);
     }
 }
--- src/main/java/com/github/mimiknight/monkey/common/utils/standard/LockService.java
+++ src/main/java/com/github/mimiknight/monkey/common/utils/standard/LockService.java
@@ -13,27 +13,58 @@ import java.util.function.Supplier;
 public interface LockService {
 
     /**
-     * 给代码块上锁
+     * 代码块上锁
+     *
+     * <p>
+     * 有时返回值
      * <p>
      * doTryLock() 在超过等待时间过后如果还未获取到锁，则获取锁失败（false）
      *
      * @param lockName   锁名
-     * @param waitTime   锁等待时间
-     * @param unit       锁时间单位
+     * @param waitTime   获取锁的等待时间
+     * @param unit       锁的时间单位
      * @param lockedCode 被上锁的代码
      * @return {@link T}
      */
     <T> T doTryLock(String lockName, long waitTime, TimeUnit unit, Supplier<T> lockedCode);
 
     /**
+     * 代码块上锁
+     * <p>
+     * 无返回值
+     * <p>
+     * doTryLock() 在超过等待时间过后如果还未获取到锁，则获取锁失败（false）
+     *
+     * @param lockName   锁名
+     * @param waitTime   获取锁的等待时间
+     * @param unit       锁的时间单位
+     * @param lockedCode 被上锁的代码
+     */
+    void doTryLock(String lockName, long waitTime, TimeUnit unit, Runnable lockedCode);
+
+    /**
      * 给代码块上锁
+     * <p>
+     * 有返回值
      * <p>
      * doLock() 如果没有获取到锁则一直等待，直到锁被释放
      *
      * @param lockName   锁名
-     * @param lockedCode 供应商
+     * @param lockedCode 被上锁的代码
      * @return {@link T}
      */
     <T> T doLock(String lockName, Supplier<T> lockedCode);
+
+    /**
+     * 给代码块上锁
+     * <p>
+     * 无返回值
+     * <p>
+     * doLock() 如果没有获取到锁则一直等待，直到锁被释放
+     *
+     * @param lockName   锁名
+     * @param lockedCode 被上锁的代码
+     */
+    void doLock(String lockName, Runnable lockedCode);
 
 }
