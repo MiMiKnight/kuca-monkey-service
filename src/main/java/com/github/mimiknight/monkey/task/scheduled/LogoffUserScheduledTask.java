26bd8caf3a16447292edffd254667cc1e7b2cadc
MiMiKnight <victor2015yhm@gmail.com>
2023-07-30 00:26:33 +0800

    【关联单号】US202307050831 【提交描述】用户注销定时任务

--- src/main/java/com/github/mimiknight/monkey/task/scheduled/LogoffUserScheduledTask.java
+++ src/main/java/com/github/mimiknight/monkey/task/scheduled/LogoffUserScheduledTask.java
@@ -5,26 +5,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
- * Hello定时任务
+ * 注销用户定时任务
  *
- * @author victor2015yhm@gmail.com
- * @since 2023-07-28 20:57:27
+ * @author MiMiKnight <victor2015yhm@gmail.com>
+ * @since 2023-07-30 00:24:11
  */
 @Slf4j
 @Component
-public class HelloScheduledTask extends BaseCronScheduledTask {
+public class LogoffUserScheduledTask extends BaseCronScheduledTask {
     @Override
     protected void doTask() {
-        log.info("Hello Scheduled Task...01");
+        log.info("Logoff User....");
     }
 
     /**
-     * 得到cron表达式
+     * 获取本定时任务的Cron表达式
+     * <p>
+     * 0 0 2 1 * ?   表示在每月1日的凌晨2点执行任务
      *
      * @return {@link String}
      */
     @Override
     public String getCronExpression() {
-        return "0/20 * * * * ? ";
+        return "0 0 2 1 * ?";
     }
 }
