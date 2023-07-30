f8740084d063803436dd5ac79ea37e95bbb76285
MiMiKnight <victor2015yhm@gmail.com>
2023-07-30 11:00:49 +0800

    【关联单号】US202307050831 【提交描述】优化注释

--- src/main/java/com/github/mimiknight/monkey/model/response/QueryArticleByIdResponse.java
+++ src/main/java/com/github/mimiknight/monkey/model/response/QueryArticleByIdResponse.java
@@ -23,45 +23,61 @@ public class QueryArticleByIdResponse implements EcologyResponse {
     /**
      * 主键
      */
-    @JsonProperty(value = "id" , index = 1)
+    @JsonProperty(value = "id", index = 1)
     private String id;
 
     /**
      * 文章标题
      */
-    @JsonProperty(value = "title" , index = 2)
+    @JsonProperty(value = "title", index = 2)
     private String title;
 
     /**
      * 文章内容
      */
-    @JsonProperty(value = "article" , index = 3)
+    @JsonProperty(value = "article", index = 3)
     private String article;
 
     /**
      * 乐观锁
      */
-    @JsonProperty(value = "version" , index = 4)
+    @JsonProperty(value = "version", index = 4)
     private Integer version;
 
     /**
      * 逻辑删除
      */
-    @JsonProperty(value = "deleted" , index = 5)
+    @JsonProperty(value = "deleted", index = 5)
     private Integer deleted;
 
     /**
      * 创建时间
+     * <p>
+     * 格式：
+     * <p>
+     * 24小时制 "年-月-日 时:分:秒.毫秒 时区"
+     * <p>
+     * 案例：
+     * <p>
+     * 2022-09-04 10:06:39.123 +08:00 【表示 东八区 2022年9月4日10点6分39秒123毫秒】
      */
     @JsonFormat(pattern = DateTimeFormatStandard.STANDARD_6, timezone = TimeZoneGMT.GMT)
-    @JsonProperty(value = "created_time" , index = 6)
+    @JsonProperty(value = "created_time", index = 6)
     private ZonedDateTime createdTime;
 
     /**
      * 更新时间
+     * <p>
+     * 格式：
+     * <p>
+     * 24小时制 "年-月-日 时:分:秒.毫秒 时区"
+     * <p>
+     * 案例：
+     * <p>
+     * 2022-09-04 10:06:39.123 +08:00 【表示 东八区 2022年9月4日10点6分39秒123毫秒】
      */
     @JsonFormat(pattern = DateTimeFormatStandard.STANDARD_6, timezone = TimeZoneGMT.GMT)
-    @JsonProperty(value = "updated_time" , index = 7)
+    @JsonProperty(value = "updated_time", index = 7)
     private ZonedDateTime updatedTime;
 
 }
