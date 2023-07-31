0023083b8327fa00244303ac32ad516b4127250a
MiMiKnight <victor2015yhm@gmail.com>
2023-07-31 22:32:54 +0800

    【关联单号】US202307050831 【提交描述】优化接口日志打印过滤器代码

--- src/main/java/com/github/mimiknight/monkey/common/spring/filter/ApiLogFilter.java
+++ src/main/java/com/github/mimiknight/monkey/common/spring/filter/ApiLogFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -29,11 +30,11 @@ public class ApiLogFilter implements Filter {
         // 如果是RepeatableReadHttpServletRequest对象才打印接口日志
         RepeatableReadHttpServletRequest servletRequest = (RepeatableReadHttpServletRequest) request;
         // 打印接口入参日志
-        log.info("打印接口入参日志");
+        traceRequest(servletRequest);
         // 放通
         chain.doFilter(servletRequest, response);
         // 打印接口出参日志
-        log.info("打印接口出参日志");
+        traceResponse(servletRequest, (HttpServletResponse) response);
     }
 
     /**
@@ -54,5 +55,24 @@ public class ApiLogFilter implements Filter {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 打印接口请求入参日志
+     *
+     * @param request 请求
+     */
+    private void traceRequest(RepeatableReadHttpServletRequest request) {
+        log.info("打印接口请求入参日志");
+    }
+
+    /**
+     * 打印接口响应出参日志
+     *
+     * @param request  请求
+     * @param response 响应
+     */
+    private void traceResponse(RepeatableReadHttpServletRequest request, HttpServletResponse response) {
+        log.info("打印接口响应出参日志");
     }
 }
