480b88f7d44fb3c7f33e6b31e8ea0e49f7550d98
MiMiKnight <victor2015yhm@gmail.com>
2023-08-02 06:23:39 +0800

    【关联单号】US202307050831 【提交描述】新增缓存、锁静态工具类

---
+++ src/main/java/com/github/mimiknight/monkey/common/utils/CacheUtils.java
@@ -0,0 +1,17 @@
+package com.github.mimiknight.monkey.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 缓存工具类
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @since 2023-08-02 06:19:58
+ */
+@Slf4j
+public class CacheUtils {
+
+    private CacheUtils() {
+    }
+
+}
---
+++ src/main/java/com/github/mimiknight/monkey/common/utils/LockUtils.java
@@ -0,0 +1,17 @@
+package com.github.mimiknight.monkey.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 锁工具类
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @since 2023-08-02 06:19:32
+ */
+@Slf4j
+public class LockUtils {
+
+    private LockUtils() {
+    }
+
+}
