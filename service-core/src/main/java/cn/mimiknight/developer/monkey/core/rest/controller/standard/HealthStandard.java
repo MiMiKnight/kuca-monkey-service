e891cd83f795dcbb3de7783fa97615daac1ac14e
MiMiKnight <victor2015yhm@gmail.com>
2024-09-02 19:48:21 +0800

    [Requirement Number]US202308182020 [Commit Description]优化

--- service-core/src/main/java/cn/mimiknight/developer/monkey/core/rest/controller/standard/HealthStandard.java
+++ service-core/src/main/java/cn/mimiknight/developer/monkey/core/rest/controller/standard/HealthStandard.java
@@ -1,8 +1,6 @@
 package cn.mimiknight.developer.monkey.core.rest.controller.standard;
 
 import cn.mimiknight.developer.monkey.core.rest.model.response.HealthCheckResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * Health模块Controller接口
@@ -10,8 +8,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
  * @author MiMiKnight victor2015yhm@gmail.com
  * @since 2023-11-11 09:35:07
  */
-@Tag(name = "健康检查模块前端控制器")
+//@Tag(name = "健康检查模块前端控制器")
 public interface HealthStandard {
-    @Operation(summary = "健康检查接口", description = "查看项目运行状态")
+    //    @Operation(summary = "健康检查接口", description = "查看项目运行状态")
     HealthCheckResponse check() throws Exception;
 }
