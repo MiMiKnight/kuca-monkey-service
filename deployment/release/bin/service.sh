11b61d34d2c5e339a1763d75c3420559712845c5
MiMiKnight <victor2015yhm@gmail.com>
2024-09-07 11:49:54 +0800

    [Requirement Number]US202308182020 [Commit Description]优化

--- deployment/release/bin/service.sh
+++ deployment/release/bin/service.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -ex
-
 # 脚本当前所在目录
 current_dir=$(cd "$(dirname "$0")" && pwd)
 # 当前脚本所在目录的上一级目录
 parent_dir=$(dirname "$current_dir")
-
 # 引入外部shell脚本
-. ./function.sh
+. func.sh
 
 ##################################
 # usage函数
