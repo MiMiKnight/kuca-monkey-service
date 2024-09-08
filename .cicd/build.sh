f749e1650f4be58a7dd5dd3cda7e68c21c60a2b9
MiMiKnight <victor2015yhm@gmail.com>
2024-09-08 19:49:57 +0800

    [Requirement Number]US202308182020 [Commit Description]优化脚本

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ex
-# 脚本当前所在目录
-current_dir=$(cd "$(dirname "$0")" && pwd)
-# 当前脚本所在目录的上一级目录
-parent_dir=$(dirname "$current_dir")
-
-# maven配置文件路径
-maven_setting_config=$1
+############全局变量常量############
+# 脚本当前所在目录 常量
+CONST_CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
+declare -r CONST_CURRENT_DIR;
+# 脚本所在的上一级目录 常量
+CONST_PARENT_DIR=$(dirname "$CONST_CURRENT_DIR")
+declare -r CONST_PARENT_DIR;
+# maven配置文件路径 全局变量
+g_maven_setting_config=$1
 
 #####################################
 ## maven package 函数
@@ -14,8 +16,8 @@ maven_setting_config=$1
 maven_package(){
   local cmd="mvn clean compile package '-Dmaven.test.skip=true'";
   # 如果外部传入的maven配置文件变量不为空且文件存在
-  if [ -z "${maven_setting_config}" ] && [ -f "${maven_setting_config}" ]; then
-    cmd="${cmd} --settings='${maven_setting_config}'"
+  if [ -z "${g_maven_setting_config}" ] && [ -f "${g_maven_setting_config}" ]; then
+    cmd="${cmd} --settings='${g_maven_setting_config}'"
   fi
   #mvn clean compile package '-Dmaven.test.skip=true' --settings="xxx/jdk8-settings.xml"
   #mvn clean compile package '-Dmaven.test.skip=true' --settings="xxx/jdk17-settings.xml"
@@ -23,14 +25,14 @@ maven_package(){
   `${cmd}`
 
   # 循环等待打包结束
-  local timeout now start_time end_time duration;
+  local -i timeout now start_time end_time duration;
   timeout=300; # 打包超时时间（单位：秒）
   now=$(date +'%Y-%m-%d %H:%M:%S');
   start_time=$(date --date="${now}" +%s);
   end_time=${start_time}
   duration=0 # 持续时间
   # 构建产物不存在时则等待构建，执行循环体；构建产物存在，则跳出循环；
-  until [ -d "${parent_dir}/.build" ]
+  until [ -d "${CONST_PARENT_DIR}/.build" ]
   do
     echo "[TIP] maven is packaging project now ...."
     now=$(date +'%Y-%m-%d %H:%M:%S')
@@ -51,32 +53,32 @@ maven_package
 ## dos2unix 函数
 #####################################
 file_dos2unix(){
-  if [ ! -d "${parent_dir}/.build" ]; then
-    echo "[TIP] ${parent_dir}/.build not exist!!!"
+  if [ ! -d "${CONST_PARENT_DIR}/.build" ]; then
+    echo "[TIP] ${CONST_PARENT_DIR}/.build not exist!!!"
     exist 0
   fi
- sudo find "${parent_dir}/.build" -type f -print0 | xargs -0 dos2unix -k -s
+ sudo find "${CONST_PARENT_DIR}/.build" -type f -print0 | xargs -0 dos2unix -k -s
 }
 
 #####################################
 ## move file 函数
 #####################################
 move_file(){
-  if [ ! -d "${parent_dir}/.build/deployment" ];then
-    echo "[TIP] ${parent_dir}/.build/deployment not exist!!!"
+  if [ ! -d "${CONST_PARENT_DIR}/.build/deployment" ];then
+    echo "[TIP] ${CONST_PARENT_DIR}/.build/deployment not exist!!!"
     exist 0
   fi
-  sudo mv -f ${parent_dir}/.build/deployment/* ${parent_dir}/.build/
-  sudo rm -rf "${parent_dir}/.build/deployment"
+  sudo mv -f ${CONST_PARENT_DIR}/.build/deployment/* ${CONST_PARENT_DIR}/.build/
+  sudo rm -rf "${CONST_PARENT_DIR}/.build/deployment"
   file_dos2unix
 }
 # 执行 move file 函数
 move_file
 
 # 项目名称
-app_name="$(awk -F '=' 'NR==2{print $2}' "${parent_dir}/.build/metadata.txt")"
+app_name="$(awk -F '=' 'NR==2{print $2}' "${CONST_PARENT_DIR}/.build/metadata.txt")"
 # 项目版本
-app_version="$(awk -F '=' 'NR==4{print $2}' "${parent_dir}/.build/metadata.txt")"
+app_version="$(awk -F '=' 'NR==4{print $2}' "${CONST_PARENT_DIR}/.build/metadata.txt")"
 # 镜像仓库用户名
 image_user="mmk"
 # 镜像仓库密码
@@ -93,13 +95,13 @@ image_tag="${image_domain}/${image_library}/${app_name}:${app_version}"
 #####################################
 build_image(){
  # 进入Dockerfile文件所在的同级目录
- cd "${parent_dir}/.build"
+ cd "${CONST_PARENT_DIR}/.build"
  # 构建docker镜像
  sudo docker build \
-  --file "${parent_dir}/.build/Dockerfile" \
+  --file "${CONST_PARENT_DIR}/.build/Dockerfile" \
   --tag "${image_tag}" .
  # 回到父级目录
- cd "${parent_dir}"
+ cd "${CONST_PARENT_DIR}"
  # 登陆docker
  sudo docker login ${image_domain} --username ${image_user} --password ${image_password}
  # 上传docker镜像
@@ -116,12 +118,12 @@ build_image
 ## 构建K8S部署 函数
 #####################################
 build_blueprint(){
-  echo "build_blueprint"
+  echo "build blueprint"
   # 替换镜像地址
-  sed -i "s@{{image_tag}}@${image_tag}@g" ${parent_dir}/.build/blueprint.yaml
+  sed -i "s@{{image_tag}}@${image_tag}@g" ${CONST_PARENT_DIR}/.build/blueprint.yaml
   # 向k8s推送部署服务
 }
 build_blueprint
 
 # 执行清除构建内容
-sudo rm -rf "${parent_dir}/.build"
+sudo rm -rf "${CONST_PARENT_DIR}/.build"
