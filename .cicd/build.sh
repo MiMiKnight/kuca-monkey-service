bed18b5db605dce406c5875058fa6a61041f3d3a
MiMiKnight <victor2015yhm@gmail.com>
2024-09-10 21:58:43 +0800

    [Requirement Number]US202308182020 [Commit Description]优化脚本变量名称

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -12,25 +12,19 @@ set -eu
 # sudo apt-get install -y jq
 # sudo apt-get install pwgen
 
-############全局变量常量############
-# 脚本当前所在目录 常量
-C_SCRIPT_CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
-declare -r C_SCRIPT_CURRENT_DIR;
-# 脚本所在的上一级目录 常量
-C_SCRIPT_PARENT_DIR=$(dirname "$C_SCRIPT_CURRENT_DIR")
-declare -r C_SCRIPT_PARENT_DIR;
+##############全局变量##############
+# 脚本当前所在目录
+script_current_dir=$(cd "$(dirname "$0")" && pwd)
+# 项目目录
+app_dir=$(dirname "$script_current_dir")
 # 镜像仓库域名
-C_IMAGE_DOMAIN=$1
-declare -r C_IMAGE_DOMAIN;
+image_domain=$1
 # 镜像仓库用户名
-C_IMAGE_USER=$2
-declare -r C_IMAGE_USER;
+image_user=$2
 # 镜像仓库密码
-C_IMAGE_PASSWORD=$3
-declare -r C_IMAGE_PASSWORD;
+image_password=$3
 # 镜像仓库名
-C_IMAGE_LIBRARY=$4
-declare -r C_IMAGE_LIBRARY;
+image_library=$4
 
 
 ##################################
@@ -85,14 +79,14 @@ MavenPackage(){
   CheckMaven
   # 构建打包命令
   local project_dir="" current_dir=""
-  project_dir=${C_SCRIPT_PARENT_DIR}
+  project_dir=${app_dir}
   # 记录当前目录
   current_dir=$(pwd)
   # 切换到项目目录
   cd "${project_dir}" || exit  1
 
   local cmd="${JAVA_HOME}/bin/java \
-  -Dmaven.multiModuleProjectDirectory=${C_SCRIPT_PARENT_DIR} \
+  -Dmaven.multiModuleProjectDirectory=${app_dir} \
   -Dmaven.home=${MAVEN_HOME} \
   -Dclassworlds.conf=${MAVEN_HOME}/bin/m2.conf \
   -Dfile.encoding=UTF-8 \
@@ -116,13 +110,13 @@ MavenPackage(){
   end_time=${start_time}
   duration=0 # 持续时间
   # 构建产物不存在时则等待构建，执行循环体；构建产物存在，则跳出循环；
-  local metadata_file_location="${C_SCRIPT_PARENT_DIR}/.build/deployment/metadata.json"
+  local metadata_file_location="${app_dir}/.build/deployment/metadata.json"
   while [ ! -f "${metadata_file_location}" ];
   do
     Info "maven is packaging project now ...."
     now=$(date +'%Y-%m-%d %H:%M:%S')
     end_time=$(date --date="$now" +%s);
-    duration=$((${end_time}-${start_time}))
+    duration=$((end_time-start_time))
     if [ ${duration} -gt ${timeout} ]; then
       Error "maven package project timeout !!!"
       exit 1 # 超时则报错退出脚本执行
@@ -137,23 +131,23 @@ MavenPackage
 ## dos2unix 函数
 #####################################
 FileDos2Unix(){
-  if [ ! -d "${C_SCRIPT_PARENT_DIR}/.build" ]; then
-    Warn "${C_SCRIPT_PARENT_DIR}/.build not exist !!!"
+  if [ ! -d "${app_dir}/.build" ]; then
+    Warn "${app_dir}/.build not exist !!!"
     exist 1
   fi
-  find "${C_SCRIPT_PARENT_DIR}/.build" -type f -print0 | xargs -0 dos2unix -k -s
+  find "${app_dir}/.build" -type f -print0 | xargs -0 dos2unix -k -s
 }
 
 #####################################
 ## move file 函数
 #####################################
 MoveFile(){
-  if [ ! -d "${C_SCRIPT_PARENT_DIR}/.build/deployment" ];then
-    Warn "${C_SCRIPT_PARENT_DIR}/.build/deployment not exist !!!"
+  if [ ! -d "${app_dir}/.build/deployment" ];then
+    Warn "${app_dir}/.build/deployment not exist !!!"
     exist 1
   fi
-  sudo cp -f ${C_SCRIPT_PARENT_DIR}/.build/deployment/* ${C_SCRIPT_PARENT_DIR}/.build/
-  sudo rm -rf "${C_SCRIPT_PARENT_DIR}/.build/deployment"
+  sudo cp -f ${app_dir}/.build/deployment/* ${app_dir}/.build/
+  sudo rm -rf "${app_dir}/.build/deployment"
   FileDos2Unix
 }
 MoveFile
@@ -166,23 +160,23 @@ BuildVersion(){
   local build_version="" cmd="";
   build_version="$(date +%Y%m%d%H%M%S)$(pwgen -ABns0 8 1 | tr a-z A-Z)"
   # 写入项目构建版本信息
-  echo "$(jq --arg value ${build_version} '.APP_BUILD_VERSION = $value' ${C_SCRIPT_PARENT_DIR}/.build/metadata.json)" > ${C_SCRIPT_PARENT_DIR}/.build/metadata.json
+  echo "$(jq --arg value ${build_version} '.APP_BUILD_VERSION = $value' ${app_dir}/.build/metadata.json)" > ${app_dir}/.build/metadata.json
 }
 BuildVersion
 
 # 项目名称
-C_APP_NAME="$(jq -r '.APP_NAME' ${C_SCRIPT_PARENT_DIR}/.build/metadata.json)"
+C_APP_NAME="$(jq -r '.APP_NAME' ${app_dir}/.build/metadata.json)"
 # 项目构建版本
-C_APP_BUILD_VERSION="$(jq -r '.APP_BUILD_VERSION' ${C_SCRIPT_PARENT_DIR}/.build/metadata.json)"
+C_APP_BUILD_VERSION="$(jq -r '.APP_BUILD_VERSION' ${app_dir}/.build/metadata.json)"
 # 项目镜像坐标
-C_IMAGE_COORDINATE="${C_IMAGE_DOMAIN}/${C_IMAGE_LIBRARY}/${C_APP_NAME}:${C_APP_BUILD_VERSION}"
+C_IMAGE_COORDINATE="${image_domain}/${image_library}/${C_APP_NAME}:${C_APP_BUILD_VERSION}"
 
 #####################################
 ## 写入元数据 函数
 #####################################
 WriteMetadata(){
   # 写入项目镜像坐标信息
-  echo "$(jq --arg value ${C_IMAGE_COORDINATE} '.APP_IMAGE_COORDINATE = $value' ${C_SCRIPT_PARENT_DIR}/.build/metadata.json)" > ${C_SCRIPT_PARENT_DIR}/.build/metadata.json
+  echo "$(jq --arg value ${C_IMAGE_COORDINATE} '.APP_IMAGE_COORDINATE = $value' ${app_dir}/.build/metadata.json)" > ${app_dir}/.build/metadata.json
 }
 WriteMetadata
 
@@ -191,21 +185,21 @@ WriteMetadata
 #####################################
 BuildImage(){
  # 进入Dockerfile文件所在的同级目录
- cd "${C_SCRIPT_PARENT_DIR}/.build" || exit 1
+ cd "${app_dir}/.build" || exit 1
  # 构建docker镜像
  sudo docker build \
-  --file "${C_SCRIPT_PARENT_DIR}/.build/Dockerfile" \
+  --file "${app_dir}/.build/Dockerfile" \
   --build-arg build_version="${C_APP_BUILD_VERSION}" \
   --build-arg timezone="Asia/Shanghai" \
   --tag "${C_IMAGE_COORDINATE}" .
  # 回到父级目录
- cd "${C_SCRIPT_PARENT_DIR}" || exit 1
+ cd "${app_dir}" || exit 1
  # 登陆docker
- sudo docker login ${C_IMAGE_DOMAIN} --username ${C_IMAGE_USER} --password ${C_IMAGE_PASSWORD}
+ sudo docker login ${image_domain} --username ${image_user} --password ${image_password}
  # 上传docker镜像
  sudo docker push "${C_IMAGE_COORDINATE}"
  # 删除产物镜像
- sudo docker rmi "$(sudo docker images | grep "${C_APP_BUILD_VERSION}" | grep "${C_IMAGE_DOMAIN}/${C_IMAGE_LIBRARY}/${C_APP_NAME}" | awk '{print $3}')"
+ sudo docker rmi "$(sudo docker images | grep "${C_APP_BUILD_VERSION}" | grep "${image_domain}/${image_library}/${C_APP_NAME}" | awk '{print $3}')"
  # 退出登陆docker
  sudo docker logout
 }
@@ -217,7 +211,7 @@ BuildImage
 BuildBlueprint(){
   Info "Start build blueprint !!!"
   # 替换镜像坐标
-  sed -i "s@image_coordinate:tag@${C_IMAGE_COORDINATE}@g" "${C_SCRIPT_PARENT_DIR}/.build/blueprint.yaml"
+  sed -i "s@image_coordinate:tag@${C_IMAGE_COORDINATE}@g" "${app_dir}/.build/blueprint.yaml"
 }
 BuildBlueprint
 
@@ -229,21 +223,21 @@ BuildDeployPackage(){
   # 记录当前目录
   current_dir=$(pwd)
   # 切换到.build目录
-  cd "${C_SCRIPT_PARENT_DIR}/.build" || exit 1
+  cd "${app_dir}/.build" || exit 1
   # 压缩包名
   archive_name="deploy-${C_APP_NAME}-${C_APP_BUILD_VERSION}.tar.gz"
   # 生成部署压缩包
   tar czf "${archive_name}" blueprint.yaml metadata.json
   # 将部署包拷贝到"部署文件夹"
-  cp -f "${C_SCRIPT_PARENT_DIR}/.build/${archive_name}" "$(dirname $C_SCRIPT_PARENT_DIR)"
+  cp -f "${app_dir}/.build/${archive_name}" "$(dirname $app_dir)"
   # 在"部署文件夹"生成deploy.json
   local json_txt="{\"DEPLOY_PACKAGE_NAME\":\"${archive_name}\"}"
-  echo "${json_txt}" >> "$(dirname $C_SCRIPT_PARENT_DIR)/deploy.json"
+  echo "${json_txt}" >> "$(dirname $app_dir)/deploy.json"
   # 切换到回原有的目录下
   cd "${current_dir}" || exit 1
 }
 BuildDeployPackage
 
 #
-rm -rf "${C_SCRIPT_PARENT_DIR}/.build"
+rm -rf "${app_dir}/.build"
 Info "The project has been built completed and success !!!"
--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -15,65 +15,65 @@ set -eu
 # sudo apt-get install pwgen
 # sudo apt-get install sshpass
 
-############全局变量常量############
-# 脚本当前所在目录 常量
-C_SCRIPT_CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
-declare -r C_SCRIPT_CURRENT_DIR;
-# 脚本所在的上一级目录 常量
-C_SCRIPT_PARENT_DIR=$(dirname "$C_SCRIPT_CURRENT_DIR")
-declare -r C_SCRIPT_PARENT_DIR;
+##############全局变量##############
+# 脚本当前所在目录
+script_current_dir=$(cd "$(dirname "$0")" && pwd)
 # 代码仓库名称
-declare -r C_REPOSITORY_NAME=$1
+repository_name=$1
 # 代码仓库地址
-declare -r C_CODE_REPOSITORY=$2
+code_repository=$2
 # 代码分支
-declare -r C_CODE_BRANCH=$3
+code_branch=$3
 # 镜像仓库域名
-declare -r C_IMAGE_DOMAIN=$4
+image_domain=$4
 # 镜像仓库用户名
-declare -r C_IMAGE_USER=$5
+image_user=$5
 # 镜像仓库密码
-declare -r C_IMAGE_PASSWORD=$6
+image_password=$6
 # 镜像仓库名
-declare -r C_IMAGE_LIBRARY=$7
+image_library=$7
 # deploy.json 文件名
-declare -r C_DEPLOY_JSON_FILE_NAME="deploy.json"
+deploy_json_file_name="deploy.json"
 
 
 ##################################
 # 友好提示函数
 ##################################
 Info() {
-  echo -e "\e[1;32;49m[INFO] \e[1;39;49m$1\e[0m";
+  now=$(date +'%Y-%m-%d %H:%M:%S')
+  echo -e "\e[1;90;49m[${now}] \e[1;32;49m[INFO] \e[1;39;49m$1\e[0m";
 }
 
 ##################################
 # 警告提示函数
 ##################################
 Warn() {
-  echo -e "\e[1;33;49m[WARN] \e[1;39;49m$1\e[0m";
+  now=$(date +'%Y-%m-%d %H:%M:%S')
+  echo -e "\e[1;90;49m[${now}] \e[1;33;49m[WARN] \e[1;39;49m$1\e[0m";
 }
 
 ##################################
 # 错误提示退出函数
 ##################################
 Error() {
-  echo -e "\e[1;31;49m[ERROR] \e[1;39;49m$1\e[0m";
+  now=$(date +'%Y-%m-%d %H:%M:%S')
+  echo -e "\e[1;90;49m[${now}] \e[1;31;49m[ERROR] \e[1;39;49m$1\e[0m";
+  exit 1
 }
 
 #####################################
 ## Check Arg 函数
 #####################################
 CheckArg(){
-  if [ -z "${C_REPOSITORY_NAME}" ]; then
+  if [ -z "${repository_name}" ]; then
       Warn "Argument \'REPOSITORY_NAME\' must not be empty !!!"
       exit 1
   fi
-  if [ -z "${C_CODE_REPOSITORY}" ]; then
+  if [ -z "${code_repository}" ]; then
       Warn "Argument \'CODE_REPOSITORY\' must not be empty !!!"
       exit 1
   fi
-  if [ -z "${C_CODE_BRANCH}" ]; then
+  if [ -z "${code_branch}" ]; then
       Warn "Argument \'CODE_BRANCH\' must not be empty !!!"
       exit 1
   fi
@@ -87,7 +87,6 @@ CheckJava(){
   local java_location="${JAVA_HOME}/bin/java"
   if [ ! -e "${java_location}" ] || [ ! -x "${java_location}" ]; then
      Error "Please install Java and set environment variables or check it !!!"
-     exit 1
   fi
 }
 
@@ -99,7 +98,6 @@ CheckMaven(){
   local mvn_location="${MAVEN_HOME}/bin/mvn"
   if [ ! -e "${mvn_location}" ] || [ ! -x "${mvn_location}" ]; then
      Error "Please install Maven and set environment variables or check it !!!"
-     exit 1
   fi
 }
 CheckMaven
@@ -112,10 +110,9 @@ GitClone(){
   Info "Start clone code !!!"
   # 目的文件夹
   local dest=$1
-  git clone "${C_CODE_REPOSITORY}" --branch "${C_CODE_BRANCH}" "${dest}"
+  git clone "${code_repository}" --branch "${code_branch}" "${dest}"
   if [ $? -ne 0 ];then
      Error "code clone failed !!!"
-     exit 1
   fi
   Info "The code clone finished and success !!!"
 }
@@ -129,8 +126,8 @@ UploadPackage(){
   Info "Start upload package !!!"
   local deploy_json_location="$1" deploy_package_location="$2";
   #sshpass -p "vagrant" scp -c "${random_dir}/${deploy_archive_name}" root@redis.dev.vm.mimiknight.cn:/home/root/${random_dir}/${deploy_archive_name}
-  #cp -f ${deploy_json_location} ${C_SCRIPT_CURRENT_DIR}
-  #cp -f ${deploy_package_location} ${C_SCRIPT_CURRENT_DIR}
+  #cp -f ${deploy_json_location} ${script_current_dir}
+  #cp -f ${deploy_package_location} ${script_current_dir}
   Info "The upload package finished and success !!!"
 }
 
@@ -141,10 +138,10 @@ Deploy(){
   Info "Start run deploy task !!!"
   # 生成随机目录名称
   local random_dir="";
-  random_dir="${C_SCRIPT_CURRENT_DIR}/$(pwgen -ABns0 16 1 | tr a-z A-Z)"
+  random_dir="${script_current_dir}/$(pwgen -ABns0 16 1 | tr a-z A-Z)"
   mkdir -p "${random_dir}"
   # 从git仓库拉取代码
-  local project_dir="${random_dir}/${C_REPOSITORY_NAME}";
+  local project_dir="${random_dir}/${repository_name}";
   GitClone "${project_dir}"
   if [ $? -ne 0 ];then
     # 删除生成的随机目录
@@ -157,7 +154,7 @@ Deploy(){
   chmod +x "${project_dir}/.cicd/build.sh"
 
   # 执行构建打包
-  /bin/bash "${project_dir}/.cicd/build.sh" "${C_IMAGE_DOMAIN}" "${C_IMAGE_USER}" "${C_IMAGE_PASSWORD}" "${C_IMAGE_LIBRARY}"
+  /bin/bash "${project_dir}/.cicd/build.sh" "${image_domain}" "${image_user}" "${image_password}" "${image_library}"
   # 如果上一步构建失败则执行if
   if [ $? -ne 0 ];then
     Warn "The build package failed !!!"
@@ -168,7 +165,7 @@ Deploy(){
 
   local deploy_json_location="" deploy_package_name="" deploy_package_location=""
   # deploy.json文件路径
-  deploy_json_location="${random_dir}/${C_DEPLOY_JSON_FILE_NAME}"
+  deploy_json_location="${random_dir}/${deploy_json_file_name}"
   # 读取部署包名
   deploy_package_name="$(jq -r '.DEPLOY_PACKAGE_NAME' "${deploy_json_location}")"
   # 部署包路径
